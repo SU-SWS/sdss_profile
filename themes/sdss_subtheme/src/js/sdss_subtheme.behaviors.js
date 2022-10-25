5e851179bd2c7d993e57313fe72790cfd97b3f15
Jen Breese-Kauth <jbreese@stanford.edu>
2022-10-25 16:53:11 -0700

    SDSS-300: JS solution for header height

--- themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
+++ themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
@@ -13,10 +13,29 @@
   attach (context, settings) {
     // console.log("Attached.");
 
-
     (function ($) {
-
       $('.su-brand-bar,.su-masthead').wrapAll('<div class="fixed-header">');
+
+      let headerheight = $('.fixed-header').outerHeight();
+      $('.dialog-off-canvas-main-canvas').css('margin-top', headerheight + 'px');
+
+      var prevWidth = window.width;
+      var observer = new ResizeObserver(function(entries) {
+      const width = entries[0].borderBoxSize?.[0].inlineSize;
+      if (typeof width === 'number' && width !== prevWidth ) {
+        prevWidth = width;
+
+        if (width <= 991) {
+          $('.dialog-off-canvas-main-canvas').css('margin-top', '');
+        } else if (width >= 991) {
+          $('.dialog-off-canvas-main-canvas').css('margin-top', headerheight + 'px');
+        }
+
+        console.log(width);
+      }
+      })
+      observer.observe(window.document.body);
+
 
     })(jQuery);
   },
