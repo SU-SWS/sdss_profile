a01f7cde0f090e42d3adebd8953b4b51e9589cd4
Jen Breese-Kauth <jbreese@stanford.edu>
2022-12-12 18:28:22 -0800

    SDSS-386: header for main and other sites

--- themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
+++ themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
@@ -18,6 +18,11 @@
 
       $('.su-brand-bar,.su-masthead').wrapAll('<div class="fixed-header">');
 
+      var sdss_logo_classes = $('#block-sdss-subtheme-branding').attr('class');
+      $('.fixed-header').addClass(sdss_logo_classes);
+
+      $('.su-brand-bar__container').append('<span><a class="sdss-brand-bar__text" href="https://sustainability.stanford.edu/">Stanford</a> | <a class="sdss-brand-bar__text" href="https://stanford.edu">Doeer School of Sustainability</a></span>');
+
     })(jQuery);
   },
 
